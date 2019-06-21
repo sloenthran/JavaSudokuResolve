84a55262b2a9010bbe44d730bb747e6d41331152
Dawid Nogacz <sloenthran@gmail.com>
2019-06-21 10:05:07 +0200

    Create project skeleton

---
+++ src/main/java/pl/nogacz/sudoku/Console.java
@@ -0,0 +1,63 @@
+package pl.nogacz.sudoku;
+
+import java.util.Scanner;
+
+/**
+ * @author Dawid Nogacz on 21.06.2019
+ */
+public class Console {
+    private Scanner console = new Scanner(System.in);
+    private boolean correctData = false;
+
+    public int getInt(int max) {
+        correctData = false;
+        int data = 0;
+
+        do {
+            if (console.hasNextInt()) {
+                data = console.nextInt();
+
+                if(data == 9) {
+                    correctData = true;
+                } else {
+                    if (data < 0 || data > max) {
+                        System.out.println("You must write valid number!");
+                        console.nextLine();
+                    } else {
+                        correctData = true;
+                    }
+                }
+            } else {
+                System.out.println("You must write number!");
+                console.nextLine();
+            }
+        } while (!correctData);
+
+        return data;
+    }
+
+    public String getString() {
+        correctData = false;
+        String data = null;
+
+        do {
+            data = console.nextLine();
+            if(data.isEmpty()) {
+                System.out.println("You must write text!");
+                System.out.println();
+            } else {
+                correctData = true;
+            }
+        } while (!correctData);
+
+        return data;
+    }
+
+    public void clearConsole() {
+        try {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
